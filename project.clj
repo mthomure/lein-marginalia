3288b50519163826fd39dfe017593929e9967aa6
Mick Thomure <mthomure@gmail.com>
2014-07-16 10:10:39 -0700

    bump marginalia version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-marginalia "0.8.0-SNAPSHOT"
+(defproject lein-marginalia "0.8.0-SNAPSHOT-yieldbot"
   :description "Leiningen plugin for Marginalia."
   :url "http://github.com/fogus/marginalia"
-  :dependencies [[marginalia "0.8.0-SNAPSHOT"]]
+  :dependencies [[marginalia "0.8.0-SNAPSHOT-yieldbot"]]
   :eval-in :leiningen)
 
