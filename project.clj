20267d5554c573b763d5b046df429e703e23d10e
Mick Thomure <mthomure@gmail.com>
2014-07-25 10:16:00 -0700

    deploy to clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject lein-marginalia "0.8.0-SNAPSHOT-yieldbot"
+(defproject org.clojars.mthomure/lein-marginalia "0.8.0-SNAPSHOT-yieldbot"
   :description "Leiningen plugin for Marginalia."
   :url "http://github.com/fogus/marginalia"
-  :dependencies [[marginalia "0.8.0-SNAPSHOT-yieldbot"]]
+  :dependencies [[org.clojars.mthomure/marginalia "0.8.0-SNAPSHOT-yieldbot"]]
   :eval-in :leiningen)
-
